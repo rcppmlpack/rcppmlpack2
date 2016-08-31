154220b74b74f5cb13808cbd237ce288e029bb10
Dirk Eddelbuettel <edd@debian.org>
2016-08-31 12:42:35 -0500

    adapted to use external MLPACK

--- src/kmeans.cpp
+++ src/kmeans.cpp
@@ -1,18 +1,22 @@
 #include "RcppMLPACK.h"
 
+#include <mlpack/core.hpp>
+#include <mlpack/methods/kmeans/kmeans.hpp>
+#include <Rcpp.h>
+
 using namespace mlpack::kmeans;
 using namespace Rcpp;
 
 // [[Rcpp::export]]
 List kmeans(const arma::mat& data, const int& clusters) {
     
-    arma::Col<size_t> assignments;
+    arma::Row<size_t> assignments;
 
-	// Initialize with the default arguments.
-	KMeans<> k;
+    // Initialize with the default arguments.
+    KMeans<> k;
 
-	k.Cluster(data, clusters, assignments); 
+    k.Cluster(data, clusters, assignments); 
 
     return List::create(_["clusters"]	= clusters,
-                        _["result"]		= assignments);
+                        _["result"]	= assignments);
 }
