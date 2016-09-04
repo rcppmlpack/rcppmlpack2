f649032c031c248bc7b583d0d700d683a870b38b
Dirk Eddelbuettel <edd@debian.org>
2016-09-03 19:00:19 -0500

    explicitly define header guards
    this prevent re-inclusion of Rcpp.h and RcppArmadillo.h

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -2,8 +2,8 @@
 // Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 #include "../inst/include/RcppMLPACK.h"
-// #include <RcppArmadillo.h>
-// #include <Rcpp.h>
+#include <RcppArmadillo.h>
+#include <Rcpp.h>
 
 using namespace Rcpp;
 
