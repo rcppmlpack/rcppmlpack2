14ea05cedae1cc6bc0fca8b6595520e57722e0ed
Dirk Eddelbuettel <edd@debian.org>
2016-10-05 08:15:02 -0500

    irowvec in function header, renaming

--- src/naive_bayes.cpp
+++ src/naive_bayes.cpp
@@ -5,16 +5,25 @@
 #include <mlpack/methods/naive_bayes/naive_bayes_classifier.hpp> 	// particular algorithm used here
 
 // [[Rcpp::export]]
-arma::irowvec naive_bayes(const arma::mat& train, const arma::mat& test, const arma::Row<size_t>& labels, const int& classes) {
+arma::irowvec naiveBayesClassifier(const arma::mat& train,
+                                   const arma::mat& test,
+                                   const arma::irowvec& labels,
+                                   const int& classes) {
     
-    arma::Row<size_t> mlp_results;
+    // MLPACK wants Row<size_t> which is an unsigned representation
+    // that R does not have
+    arma::Row<size_t> labelsur, resultsur;
+
+    // TODO: check that all values are non-negative
+    labels_ = arma::conv_to<arma::Row<size_t>>::from(labels);
 
     // Initialize with the default arguments.
-    mlpack::naive_bayes::NaiveBayesClassifier<> nbc(train, labels, classes);
+    // TODO: support more arguments>
+    mlpack::naive_bayes::NaiveBayesClassifier<> nbc(train, labels_, classes);
     
-    nbc.Classify(test, mlp_results);
+    nbc.Classify(test, results_);
     
-    arma::irowvec results = arma::conv_to<arma::irowvec>::from(mlp_results);
+    arma::irowvec results = arma::conv_to<arma::irowvec>::from(results_);
     
     return results;
 }
