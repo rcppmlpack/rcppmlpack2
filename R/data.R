1cc74fd7dc41c51ab2c1613367b0423fc1b3c178
Dirk Eddelbuettel <edd@debian.org>
2017-02-21 21:19:41 -0600

    four data file added from mlpack upstream

---
+++ R/data.R
@@ -0,0 +1,35 @@
+##' Training data from the mlpack unit tests
+##'
+##' @docType data
+##' @name trainSet
+##' @format A \sQuote{csv} file read in and \emph{transposed} by
+##'  \code{mlpack} conventions.
+##' @source \url{https://raw.githubusercontent.com/mlpack/mlpack/master/src/mlpack/tests/data/trainSet.csv}
+NULL
+
+##' Training data result from the mlpack unit tests
+##'
+##' @docType data
+##' @name trainRes
+##' @format A \sQuote{csv} file read in and \emph{transposed} by
+##'  \code{mlpack} conventions.
+##' @source \url{https://raw.githubusercontent.com/mlpack/mlpack/master/src/mlpack/tests/data/trainRes.csv}
+NULL
+
+##' Test data from the mlpack unit tests
+##'
+##' @docType data
+##' @name testSet
+##' @format A \sQuote{csv} file read in and \emph{transposed} by
+##'  \code{mlpack} conventions.
+##' @source \url{https://raw.githubusercontent.com/mlpack/mlpack/master/src/mlpack/tests/data/testSet.csv}
+NULL
+
+##' Test data result from the mlpack unit tests
+##'
+##' @docType data
+##' @name testRes
+##' @format A \sQuote{csv} file read in and \emph{transposed} by
+##'  \code{mlpack} conventions.
+##' @source \url{https://raw.githubusercontent.com/mlpack/mlpack/master/src/mlpack/tests/data/testRes.csv}
+NULL
